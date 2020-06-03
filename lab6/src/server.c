f96b070be392e1e81f1e320cb2b2cd4cd7c4e27e
diman2168 <dima216@bk.ru>
2020-06-03 22:20:28 +0000

    task1

--- lab6/src/server.c
+++ lab6/src/server.c
@@ -13,24 +13,14 @@
 
 #include "pthread.h"
 
+#include "MultMod.h"
+
 struct FactorialArgs {
   uint64_t begin;
   uint64_t end;
   uint64_t mod;
 };
 
-uint64_t MultModulo(uint64_t a, uint64_t b, uint64_t mod) {
-  uint64_t result = 0;
-  a = a % mod;
-  while (b > 0) {
-    if (b % 2 == 1)
-      result = (result + a) % mod;
-    a = (a * 2) % mod;
-    b /= 2;
-  }
-
-  return result % mod;
-}
 
 uint64_t Factorial(const struct FactorialArgs *args) {
   uint64_t ans = 1;
